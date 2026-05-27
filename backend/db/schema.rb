56158a411ad293de2ce56a90cd9018fc264cb6dc
Robertas Cvetinskis <robertas.cvetinskis@gmail.com>
2026-05-27 21:52:38 +0300

    init company table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_05_27_162146) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_27_184249) do
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "location", null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,8 +27,11 @@ ActiveRecord::Schema[7.0].define(version: 2026_05_27_162146) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "company_id"
+    t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "users", "companies"
 end
